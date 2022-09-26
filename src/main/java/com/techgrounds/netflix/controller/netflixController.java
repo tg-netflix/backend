aa5dfe91770609ded625d1dde69513a1bbaf9a21
MajaMana <marijanam-98@hotmail.com>
2022-09-26 14:11:40 +0200

    hello Netflix Project

---
+++ src/main/java/com/techgrounds/netflix/controller/netflixController.java
@@ -0,0 +1,12 @@
+package com.techgrounds.netflix.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class netflixController {
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello Netflix Project";
+    }
+}
