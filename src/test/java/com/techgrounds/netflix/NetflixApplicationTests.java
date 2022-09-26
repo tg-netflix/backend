08c8f2ac4684a6791e20827bfc8428477b78f864
MajaMana <marijanam-98@hotmail.com>
2022-09-26 13:43:16 +0200

    springboot initializer

---
+++ src/test/java/com/techgrounds/netflix/NetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techgrounds.netflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
