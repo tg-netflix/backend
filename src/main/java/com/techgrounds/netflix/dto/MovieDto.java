2e58555d95d7ed2d8137cceae9e6287a1d56ae54
Hiu-Chee  Sin <aggutino@hotmail.com>
2022-11-01 10:24:38 +0100

    browse backdrop_path with fallback

--- src/main/java/com/techgrounds/netflix/dto/MovieDto.java
+++ src/main/java/com/techgrounds/netflix/dto/MovieDto.java
@@ -8,6 +8,14 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MovieDto {
+    public void setBackdrop_path(String backdrop_path) {
+        if(backdrop_path == null){
+            this.backdrop_path = "https://images3.alphacoders.com/678/678085.jpg";
+        }else {
+            this.backdrop_path = "https://image.tmdb.org/t/p/original" + backdrop_path;
+        }
+    }
+
     private String backdrop_path;
     private String id;
     private String title;
